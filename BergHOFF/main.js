d88a07a54e80a513b39b5f0d22f3abeaa5ce3046
KobeHenkens <kobe.henkens@outlook.com>
2024-05-15 22:40:41 +0200

    Update
    Setup boilerplates
    Canvas setup + responsive

---
+++ BergHOFF/main.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(){
+  const canvas = document.getElementById('game');
+  const ctx = canvas.getContext('2d');
+  canvas.width = 1920;
+  canvas.height = 1080;
+})
